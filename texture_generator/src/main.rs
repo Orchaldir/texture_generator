7ea37bbc70ebb05c101b6eff15913c23a8612c39
Orchaldir <orchaldir@gmail.com>
2021-03-07 16:51:20 +0100

    [#1] Use structopt for CLI arguments

--- texture_generator/src/main.rs
+++ texture_generator/src/main.rs
@@ -1,3 +1,18 @@
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+struct Cli {
+    /// The path of the output image.
+    #[structopt(parse(from_os_str))]
+    output: std::path::PathBuf,
+
+    /// The size of the output image.
+    #[structopt(default_value = "1024")]
+    size: u32,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::from_args();
+
+    println!("output={:?} size={}", args.output, args.size)
 }
