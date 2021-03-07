745d8e0bc787a8c8f71308c3145bea1198125cdb
Orchaldir <orchaldir@gmail.com>
2021-03-07 19:39:10 +0100

    [#1] Use crate image to save images

--- texture_generator/src/main.rs
+++ texture_generator/src/main.rs
@@ -14,5 +14,13 @@ struct Cli {
 fn main() {
     let args = Cli::from_args();
 
-    println!("output={:?} size={}", args.output, args.size)
+    println!(
+        "Save image of size {0}*{0} to {1:?}",
+        args.size, args.output
+    );
+
+    let pink = image::Rgb([255u8, 0, 128]);
+    let image = image::ImageBuffer::from_pixel(args.size, args.size, pink);
+
+    image.save(&args.output).unwrap();
 }
