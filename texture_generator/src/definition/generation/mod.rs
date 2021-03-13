49043a78e90d4f1c3b0c634cf2e5066cf4826daa
Orchaldir <orchaldir@gmail.com>
2021-03-13 09:55:56 +0100

    [#3] Add TextureDefinition & TextureGenerator

--- texture_generator/src/definition/generation/mod.rs
+++ texture_generator/src/definition/generation/mod.rs
@@ -1,3 +1,71 @@
+use crate::definition::generation::component::ComponentDefinition;
+use crate::generation::TextureGenerator;
+use crate::utils::error::GenerationError;
+use std::convert::{TryFrom, TryInto};
+
 pub mod component;
 pub mod layout;
 pub mod rendering;
+
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub struct TextureDefinition {
+    name: String,
+    component: ComponentDefinition,
+}
+
+impl TextureDefinition {
+    pub fn new<S: Into<String>>(name: S, component: ComponentDefinition) -> TextureDefinition {
+        TextureDefinition {
+            name: name.into(),
+            component,
+        }
+    }
+}
+
+impl TryFrom<TextureDefinition> for TextureGenerator {
+    type Error = GenerationError;
+
+    fn try_from(definition: TextureDefinition) -> Result<Self, Self::Error> {
+        Ok(TextureGenerator::new(
+            definition.name,
+            definition.component.try_into()?,
+        ))
+    }
+}
+
+impl From<&TextureGenerator> for TextureDefinition {
+    fn from(generator: &TextureGenerator) -> Self {
+        TextureDefinition::new(generator.name.clone(), (&generator.component).into())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::definition::generation::component::ComponentDefinition;
+    use crate::definition::generation::rendering::RenderingDefinition;
+    use crate::definition::math::shape::ShapeDefinition;
+    use crate::math::color::RED;
+    use std::convert::TryInto;
+
+    const SHAPE: ShapeDefinition = ShapeDefinition::Circle(42);
+
+    #[test]
+    fn test_convert_layout() {
+        let rendering = RenderingDefinition::Shape {
+            name: "brick".to_string(),
+            shape: SHAPE,
+            color: RED,
+        };
+        let component = ComponentDefinition::Rendering(Box::new(rendering));
+
+        assert_convert(TextureDefinition::new("test", component));
+    }
+
+    fn assert_convert(definition: TextureDefinition) {
+        let generator: TextureGenerator = definition.clone().try_into().unwrap();
+        let result: TextureDefinition = (&generator).into();
+
+        assert_eq!(result, definition)
+    }
+}
--- texture_generator/src/generation/mod.rs
+++ texture_generator/src/generation/mod.rs
@@ -1,4 +1,21 @@
+use crate::generation::component::Component;
+
 pub mod component;
 pub mod data;
 pub mod layout;
 pub mod rendering;
+
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub struct TextureGenerator {
+    pub name: String,
+    pub component: Component,
+}
+
+impl TextureGenerator {
+    pub fn new<S: Into<String>>(name: S, component: Component) -> TextureGenerator {
+        TextureGenerator {
+            name: name.into(),
+            component,
+        }
+    }
+}
