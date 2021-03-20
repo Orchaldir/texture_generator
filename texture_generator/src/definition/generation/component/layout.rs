6d7dceafaf2942673bbe3402e505500c1158dc95
Orchaldir <orchaldir@gmail.com>
2021-03-20 11:02:10 +0100

    [#12] Add LayoutDefinition::BrickWall

--- texture_generator/src/definition/generation/component/layout.rs
+++ texture_generator/src/definition/generation/component/layout.rs
@@ -1,11 +1,18 @@
-use crate::definition::convert;
 use crate::definition::generation::component::ComponentDefinition;
+use crate::definition::{convert, convert_size};
 use crate::generation::component::layout::LayoutComponent;
+use crate::math::size::Size;
 use crate::utils::error::GenerationError;
 use serde::{Deserialize, Serialize};
 
 #[derive(Clone, Debug, Eq, PartialEq, Serialize, Deserialize)]
 pub enum LayoutDefinition {
+    BrickWall {
+        name: String,
+        brick: Size,
+        offset: u32,
+        component: ComponentDefinition,
+    },
     Square {
         name: String,
         side: u32,
@@ -16,6 +23,20 @@ pub enum LayoutDefinition {
 impl LayoutDefinition {
     pub fn convert(&self, factor: f32) -> Result<LayoutComponent, GenerationError> {
         match self {
+            LayoutDefinition::BrickWall {
+                name,
+                brick,
+                offset,
+                component,
+            } => match component.convert(factor) {
+                Ok(component) => LayoutComponent::new_brick_wall(
+                    name,
+                    convert_size(brick, factor),
+                    convert(*offset, factor),
+                    component,
+                ),
+                Err(error) => Err(error),
+            },
             LayoutDefinition::Square {
                 name,
                 side,
@@ -34,6 +55,21 @@ impl LayoutDefinition {
 mod tests {
     use super::*;
     use crate::generation::component::Component;
+
+    #[test]
+    fn test_convert_brick_wall() {
+        let definition = LayoutDefinition::BrickWall {
+            name: "test".to_string(),
+            brick: Size::new(20, 10),
+            offset: 10,
+            component: ComponentDefinition::Mock(66),
+        };
+        let component =
+            LayoutComponent::new_brick_wall("test", Size::new(40, 20), 20, Component::Mock(66))
+                .unwrap();
+
+        assert_eq!(component, definition.convert(2.0).unwrap())
+    }
 
     #[test]
     fn test_convert_square() {
