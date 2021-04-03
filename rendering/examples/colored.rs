8aac9beb9dd29480f2e5f79bd9b066064acd62fd
Orchaldir <orchaldir@gmail.com>
2021-04-03 12:53:09 +0200

    [#43] Add examples

---
+++ rendering/examples/colored.rs
@@ -0,0 +1,34 @@
+extern crate glium;
+extern crate rendering;
+
+use rendering::implementation::window::GliumWindow;
+use rendering::interface::app::App;
+use rendering::interface::rendering::Renderer;
+use rendering::interface::window::Window;
+use rendering::interface::{BLUE, GREEN, RED, YELLOW};
+use std::cell::RefCell;
+use std::rc::Rc;
+
+#[derive(Default)]
+pub struct ColoredExample {}
+
+impl App for ColoredExample {
+    fn render(&mut self, renderer: &mut dyn Renderer) {
+        renderer.start(BLUE);
+
+        let color_renderer = renderer.get_color_renderer();
+        color_renderer.render_triangle((400.0, 300.0), (600.0, 300.0), (500.0, 400.0), GREEN);
+        color_renderer.render_triangle((100.0, 300.0), (300.0, 300.0), (200.0, 400.0), RED);
+        color_renderer.render_rectangle((300.0, 40.0), (140.0, 50.0), YELLOW);
+        color_renderer.render_rectangle((300.0, 500.0), (140.0, 50.0), GREEN);
+
+        renderer.finish();
+    }
+}
+
+fn main() {
+    let mut window = GliumWindow::default_size("Example with colored Polygons");
+    let app = Rc::new(RefCell::new(ColoredExample::default()));
+
+    window.run(app.clone());
+}
---
+++ resources/shader/colored.fragment
@@ -0,0 +1,8 @@
+#version 140
+
+in vec3 v_color;
+out vec4 color;
+
+void main() {
+    color = vec4(v_color, 1.0);
+}
