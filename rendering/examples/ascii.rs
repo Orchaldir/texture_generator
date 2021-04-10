f04f9071c2ed0316aa2c7bc6a66588d7d5ff6cf9
Orchaldir <orchaldir@gmail.com>
2021-04-10 11:46:30 +0200

    [#33] Fix example

--- rendering/examples/ascii.rs
+++ rendering/examples/ascii.rs
@@ -38,7 +38,7 @@ impl App for AsciiExample {
         renderer.finish();
 
         if self.take_screenshot {
-            info!("Take screenshot");
+            println!("Take screenshot");
             renderer.take_screenshot("ascii.png");
             self.take_screenshot = false;
         }
