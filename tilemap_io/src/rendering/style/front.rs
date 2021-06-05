941caa1651b6d0764687578ea8a79f6af49c0baa
Orchaldir <orchaldir@gmail.com>
2021-06-05 20:24:42 +0200

    [#101] Fix convert

--- tilemap_io/src/rendering/style/front.rs
+++ tilemap_io/src/rendering/style/front.rs
@@ -1,5 +1,6 @@
 use anyhow::{Context, Result};
 use serde::{Deserialize, Serialize};
+use texture_generation::definition::convert;
 use tilemap::rendering::style::front::FrontStyle;
 
 #[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
@@ -11,12 +12,14 @@ pub enum FrontDefinition {
 }
 
 impl FrontDefinition {
-    pub fn convert(&self, parent: &str, _factor: f32) -> Result<FrontStyle> {
+    pub fn convert(&self, parent: &str, factor: f32) -> Result<FrontStyle> {
         Ok(match self {
             FrontDefinition::None => FrontStyle::None,
             FrontDefinition::One(door_id) => FrontStyle::One(*door_id),
-            FrontDefinition::Repeat { step, door_id } => FrontStyle::new_repeat(*step, *door_id)
-                .context(format!("Failed to create '{}.Repeat'", parent))?,
+            FrontDefinition::Repeat { step, door_id } => {
+                FrontStyle::new_repeat(convert(*step, factor), *door_id)
+                    .context(format!("Failed to create '{}.Repeat'", parent))?
+            }
             FrontDefinition::Split(entries) => FrontStyle::new_split(entries.clone())
                 .context(format!("Failed to create '{}.Split'", parent))?,
         })
@@ -30,7 +33,7 @@ mod tests {
     #[test]
     fn test_convert_none() {
         assert_eq!(
-            FrontDefinition::None.convert("test", 1.0).unwrap(),
+            FrontDefinition::None.convert("test", 2.0).unwrap(),
             FrontStyle::None
         );
     }
@@ -38,7 +41,7 @@ mod tests {
     #[test]
     fn test_convert_one() {
         assert_eq!(
-            FrontDefinition::One(42).convert("test", 1.0).unwrap(),
+            FrontDefinition::One(42).convert("test", 2.0).unwrap(),
             FrontStyle::One(42)
         );
     }
@@ -50,16 +53,16 @@ mod tests {
             door_id: 5,
         };
         let style = FrontStyle::Repeat {
-            step: 10,
+            step: 25,
             door_id: 5,
         };
-        assert_eq!(definition.convert("test", 1.0).unwrap(), style);
+        assert_eq!(definition.convert("test", 2.5).unwrap(), style);
     }
 
     #[test]
     fn test_convert_split() {
         let definition = FrontDefinition::Split(vec![(3, Some(10)), (3, None)]);
         let style = FrontStyle::Split(vec![(0.5, Some(10)), (0.5, None)]);
-        assert_eq!(definition.convert("test", 1.0).unwrap(), style);
+        assert_eq!(definition.convert("test", 2.0).unwrap(), style);
     }
 }
