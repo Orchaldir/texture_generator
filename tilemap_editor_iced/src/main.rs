00142408cf09bc989e7beec7ecd1ae0f71bcbeae
Orchaldir <orchaldir@gmail.com>
2021-06-11 21:32:45 +0200

    [#115] Test iced 3

--- tilemap_editor_iced/src/main.rs
+++ tilemap_editor_iced/src/main.rs
@@ -1,10 +1,18 @@
+#[macro_use]
+extern crate log;
+use crate::resources::ResourceInfo;
 use iced::{image, Column, Element, Sandbox, Settings, Text};
+use structopt::StructOpt;
+use texture_generation::math::color::convert_bgra;
+
+mod resources;
 
 pub fn main() -> iced::Result {
     Hello::run(Settings::default())
 }
 
 struct Hello {
+    resource_info: ResourceInfo,
     image: image::Handle,
     image_viewer: image::viewer::State,
 }
@@ -13,8 +21,17 @@ impl Sandbox for Hello {
     type Message = ();
 
     fn new() -> Hello {
+        let resource_info: ResourceInfo = ResourceInfo::from_args();
+        let (renderer, preview_renderer) = resource_info.create_renderers();
+        let (tilemap, furniture_map) = resource_info.load_tilemap();
+
+        let data = preview_renderer.render(&tilemap, Some(&furniture_map));
+        let rbg = convert_bgra(data.get_color_data());
+        let size = data.get_size();
+
         Hello {
-            image: image::Handle::from("tilemap-color.png"),
+            resource_info,
+            image: image::Handle::from_pixels(size.width(), size.height(), rbg),
             image_viewer: image::viewer::State::new(),
         }
     }
