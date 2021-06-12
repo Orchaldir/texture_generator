f7e1a90b8b2ae143930215a616ffcfea89db16d8
Orchaldir <orchaldir@gmail.com>
2021-06-12 22:57:37 +0200

    [#115] Improve Tools

--- tilemap_editor_iced/src/tool/tools.rs
+++ tilemap_editor_iced/src/tool/tools.rs
@@ -6,45 +6,36 @@ use crate::tool::Tool;
 use iced::Element;
 
 pub struct Tools {
-    tile_tool: TileTool,
-    wall_tool: WallTool,
+    tools: Vec<Box<dyn Tool>>,
     current_tool: usize,
 }
 
 impl Tools {
     pub fn new() -> Self {
         Tools {
-            tile_tool: TileTool::default(),
-            wall_tool: WallTool::default(),
+            tools: vec![Box::new(TileTool::default()), Box::new(WallTool::default())],
             current_tool: 0,
         }
     }
 
     pub fn get_tool_names(&self) -> Vec<&str> {
-        vec![self.tile_tool.get_name(), self.wall_tool.get_name()]
+        self.tools.iter().map(|t| t.get_name()).collect()
     }
 
     pub fn get_current_tool(&self) -> usize {
         self.current_tool
     }
 
-    fn get_tool_mut(&mut self) -> &mut dyn Tool {
-        match self.current_tool {
-            1 => &mut self.wall_tool,
-            _ => &mut self.tile_tool,
-        }
-    }
-
     pub fn update(&mut self, data: &mut EditorData, message: EditorMessage) -> bool {
         match message {
             EditorMessage::ChangeTool(id) => self.current_tool = id,
-            _ => return self.get_tool_mut().update(data, message),
+            _ => return self.tools[self.current_tool].update(data, message),
         }
 
         return false;
     }
 
     pub fn view_sidebar(&mut self, data: &EditorData) -> Element<EditorMessage> {
-        self.get_tool_mut().view_sidebar(data)
+        self.tools[self.current_tool].view_sidebar(data)
     }
 }
