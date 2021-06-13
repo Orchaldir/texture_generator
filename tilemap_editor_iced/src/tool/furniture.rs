c70155e23515cfba747432e66cc2095cab10daf8
Orchaldir <orchaldir@gmail.com>
2021-06-13 13:34:01 +0200

    [#115] Improve FurnitureTool

--- tilemap_editor_iced/src/tool/furniture.rs
+++ tilemap_editor_iced/src/tool/furniture.rs
@@ -2,7 +2,7 @@ use crate::data::EditorData;
 use crate::message::EditorMessage;
 use crate::tool::{create_pick_list, Tool};
 use iced::mouse::Button;
-use iced::{pick_list, Column, Element, Text};
+use iced::{pick_list, Column, Element, HorizontalAlignment, Length, Text};
 use texture_generation::math::point::Point;
 
 #[derive(Clone, Debug, Default)]
@@ -20,12 +20,23 @@ impl Tool for FurnitureTool {
     fn update(&mut self, data: &mut EditorData, message: EditorMessage) -> bool {
         match message {
             EditorMessage::ChangeFurnitureStyle(name) => {
-                if let Some(id) = data.renderer.get_resources().furniture_styles.get_id(&name) {
+                if let Some(style_id) = data.renderer.get_resources().furniture_styles.get_id(&name)
+                {
+                    if let Some(id) = self.selected_id {
+                        if let Some(furniture) = data.furniture_map.get_furniture_mut(id) {
+                            info!(
+                                "FurnitureTool: Change furniture {}'s style to '{}' with id {}",
+                                id, &name, style_id
+                            );
+                            furniture.style_id = style_id;
+                            return true;
+                        }
+                    }
                     info!(
                         "FurnitureTool: Change furniture style to '{}' with id {}",
-                        &name, id
+                        &name, style_id
                     );
-                    self.furniture_style_id = id;
+                    self.furniture_style_id = style_id;
                 }
             }
             EditorMessage::ClickedButton { x, y, button } => {
@@ -43,9 +54,12 @@ impl Tool for FurnitureTool {
                             }
                             _ => {
                                 info!("FurnitureTool: Remove furniture with id {}", id);
+                                self.selected_id = None;
                                 data.furniture_map.remove_furniture(id)
                             }
                         };
+                    } else {
+                        self.selected_id = None;
                     }
                 }
             }
@@ -56,6 +70,28 @@ impl Tool for FurnitureTool {
     }
 
     fn view_sidebar(&mut self, data: &EditorData) -> Element<'_, EditorMessage> {
+        if let Some(id) = self.selected_id {
+            let pick_list = create_pick_list(
+                &data.renderer.get_resources().furniture_styles,
+                &mut self.furniture_style_state,
+                id,
+                EditorMessage::ChangeFurnitureStyle,
+            );
+
+            return Column::new()
+                .max_width(250)
+                .spacing(20)
+                .push(
+                    Text::new("Furniture")
+                        .width(Length::Fill)
+                        .horizontal_alignment(HorizontalAlignment::Center),
+                )
+                .push(Text::new(format!("Id = {}", id)))
+                .push(Text::new("Style"))
+                .push(pick_list)
+                .into();
+        }
+
         let pick_list = create_pick_list(
             &data.renderer.get_resources().furniture_styles,
             &mut self.furniture_style_state,
@@ -64,7 +100,7 @@ impl Tool for FurnitureTool {
         );
 
         Column::new()
-            .max_width(800)
+            .max_width(250)
             .spacing(20)
             .push(Text::new("Furniture Style"))
             .push(pick_list)
